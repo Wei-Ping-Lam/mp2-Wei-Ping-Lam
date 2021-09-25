cb436805f3ebabde5308ef2df8c8e58a88cd1659
Wei-Ping-Lam <wnlam2@illinois.edu>
2021-09-25 00:19:56 -0500

    Worked on Animal Exercise

--- DesignPatterns/src/main/java/AnimalExercise/Duck.java
+++ DesignPatterns/src/main/java/AnimalExercise/Duck.java
@@ -2,10 +2,6 @@ package AnimalExercise;
 
 public class Duck extends Animal {
 
-    Duck(String backpack) {
-        super(backpack);
-    }
-
     public String speak() {
         return "Quack";
     }
